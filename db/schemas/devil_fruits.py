08bb0fff781cf58d9bae8ae182055e522b46965a
manumafe98 <manumaxerapro@gmail.com>
2023-06-14 19:13:58 -0300

    Adding schemas and starting to make the endpoints work

---
+++ db/schemas/devil_fruits.py
@@ -0,0 +1,8 @@
+def devil_fruit_schema(devil_fruit) -> dict:
+    return {"devil_fruit_name": devil_fruit["devil_fruit_name"],
+            "devil_fruit_type": devil_fruit["devil_fruit_type"],
+            "current_user": devil_fruit["current_user"],
+            "devil_fruit_img": devil_fruit["devil_fruit_img"]}
+
+def devil_fruits_schema():
+    pass
--- routers/devil_fruits.py
+++ routers/devil_fruits.py
@@ -1,33 +1,44 @@
 from fastapi import APIRouter, HTTPException, status
+from db.models.devil_fruits import DevilFruitsModel
 from db.client import db_client
+from db.schemas.devil_fruits import devil_fruit_schema, devil_fruits_schema
 
-router = APIRouter(prefix="/devil_fruits", tags=["devil_fruits"])
+router = APIRouter(prefix="/devil_fruits", 
+                   tags=["devil_fruits"], 
+                   responses={status.HTTP_404_NOT_FOUND: {"error": "Something went wrong"}})
 
-router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 async def get_all_devil_fruits():
     return {}
 
 
-router.post("/")
-async def add_a_devil_fruit():
+@router.post("/", response_model=DevilFruitsModel, status_code=status.HTTP_201_CREATED)
+async def add_a_devil_fruit(devil_fruit: DevilFruitsModel):
+
+    devil_fruit_dict = dict(devil_fruit)
+    id = db_client.devil_fruits.insert_one(devil_fruit_dict).inserted_id
+
+    new_devil_fruit = devil_fruit_schema(db_client.devil_fruits.find_one({"_id": id}))
+
+    return DevilFruitsModel(**new_devil_fruit)
+
+
+@router.get("/{devil_fruit}", response_model=DevilFruitsModel, status_code=status.HTTP_200_OK)
+async def get_a_devil_fruit(devil_fruit: str):
+    db_client.devil_fruits.find_one({"devil_fruit_name": devil_fruit})
     return {}
 
 
-router.get("/{devil_fruit}")
-async def get_a_devil_fruit():
+@router.delete("/{devil_fruit}", status_code=status.HTTP_200_OK)
+async def delete_a_devil_fruit(devil_fruit: str):
     return {}
 
 
-router.delete("/{devil_fruit}")
-async def delete_a_devil_fruit():
+@router.put("/{devil_fruit}", status_code=status.HTTP_200_OK)
+async def update_a_devil_fruit(devil_fruit: str):
     return {}
 
 
-router.put("/{devil_fruit}")
-async def update_a_devil_fruit():
-    return {}
-
-
-router.get("/{devil_fruit}")
-async def patch_a_devil_fruit():
+@router.get("/{devil_fruit}", status_code=status.HTTP_200_OK)
+async def patch_a_devil_fruit(devil_fruit: str):
     return {}
